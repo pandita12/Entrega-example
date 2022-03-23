4e29a32f4468599451c34fe4d92960ee2b924f68
pandita12 <swodorheranndez@gmail.com>
2022-03-23 15:02:10 -0400

    actualizacion de los modelos de las apps por separado

--- apps/lesson/models.py
+++ apps/lesson/models.py
@@ -1,22 +1,38 @@
 from django.db import models
 
 from apps.consumer.models import User
+from apps.teacherbot.models import Professor
 # Create your models here.
 
-class Materia(models.Model):
-    codigo = models.CharField(max_length=6, primary_key=True)
+class Matter(models.Model):
+    code = models.CharField(max_length=6, primary_key=True)
     name_m = models.CharField(max_length=30)
-    usuario_id = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
 
     
-class Clase(models.Model):
-    usuario_id = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
-    materia_id = models.ForeignKey(Materia, null=False, blank=False, on_delete=models.CASCADE)
+class Classroom(models.Model):
+    matter_id = models.ForeignKey(Matter, null=False, blank=False, on_delete=models.CASCADE)
+    professor_id = models.ForeignKey(Professor, null=False, blank=False, on_delete=models.CASCADE)
+    classperiod = models.DateTimeField(default=False)
+    stardate = models.DateTimeField()
+    finaldate = models.DateTimeField()
+    section = models.CharField(max_length=5)
+
+class Students(models.Model):
+    matricula = models,CharField(max_length=7, primary_key=True)
+    users = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
+    matter_id = models.ForeignKey(Matter, null=False, blank=False, on_delete=models.CASCADE)
+    classroom_id = models.ForeignKey(Classroom, null=False, blank=False, on_delete=models.CASCADE)
+    
+
+class Enrollment(models.Model):
+    classroom_id = models.ForeignKey(Classroom, null=False, blank=False, on_delete=models.CASCADE)
+    students_id = models.ForeignKey(Students, null=False, blank=False, on_delete=models.CASCADE)
+    registration_date = models.DateTimeField()
+    status = models.BooleanField(max_length=1)
+    name = models.CharField(max_length=15)
+    
 
 
-class Est_grupo(models.Model):
-    usuario_id = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
-    clase_id = models.ForeignKey(Clase, null=False, blank=False, on_delete=models.CASCADE)
 
 
 
--- apps/teacherbot/models.py
+++ apps/teacherbot/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
-from apps.consumer.models import User
-from apps.lesson.models import Clase, Materia
+from apps.lesson.models import Classroom, Matter, Students
 # Create your models here.
 
 
-class Profesor(models.Model):
-    clase_id = models.ForeignKey(Clase, null=False, blank=False, on_delete=models.CASCADE)
-    usuario_id = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
-    materia_id = models.ForeignKey(Materia, null=False, blank=False, on_delete=models.CASCADE)
+class Professor(models.Model):
+    classroom_id = models.ForeignKey(Classroom, null=False, blank=False, on_delete=models.CASCADE)
+    students_id = models.ForeignKey(Students, null=False, blank=False, on_delete=models.CASCADE)
+    matter_id = models.ForeignKey(Matter, null=False, blank=False, on_delete=models.CASCADE)
+    name_teacher = models.CharField(max_length=15)
 
 
 class Config_bot(models.Model):
-    timefin = models.DateField()
-    contenido_id = models.FileField(max_length=10)
-    profesor_id = models.OneToOneField(Profesor, null=False, blank=False, on_delete=models.CASCADE)
+    timefin = models.DateTimeField()
+    content = models.FileField(max_length=15)
+    professor = models.OneToOneField(Professor, null=False, blank=False, on_delete=models.CASCADE)
